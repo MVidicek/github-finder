0e183e112b805706d14348a706004d383fc129ba
MVidicek <mvidicek@protonmail.com>
2022-06-12 00:35:41 +0200

    Search users

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -18,25 +18,30 @@ export function GithubProvider({ children }) {
   // Set loading
   const setLoading = () => dispatch({ type: "SET_LOADING" });
 
-  // Get initial users (testing purposes)
-  const fetchUsers = async () => {
+  // Get search results
+  const searchUsers = async (text) => {
     setLoading();
-    const response = await fetch(`${GITHUB_URL}/users`, {
+
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
-    const data = await response.json();
+    const { items } = await response.json();
 
     dispatch({
       type: "GET_USERS",
-      payload: data,
+      payload: items,
     });
   };
 
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, fetchUsers }}
+      value={{ users: state.users, loading: state.loading, searchUsers }}
     >
       {children}
     </GithubContext.Provider>
