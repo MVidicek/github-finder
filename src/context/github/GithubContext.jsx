b2523e1f45fbff9460238389fd73baa2dc94cb9a
MVidicek <mvidicek@protonmail.com>
2022-06-13 19:37:51 +0200

    Refactoring to Action file

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -4,9 +4,6 @@ import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-
 export function GithubProvider({ children }) {
   const initialState = {
     users: [],
@@ -17,63 +14,11 @@ export function GithubProvider({ children }) {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // Set loading
-  const setLoading = () => dispatch({ type: "SET_LOADING" });
-
-  // Clear users
-  const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
-
-  // Get search results
-  const searchUsers = async (text) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      q: text,
-    });
-
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-    const { items } = await response.json();
-
-    dispatch({
-      type: "GET_USERS",
-      payload: items,
-    });
-  };
-
-  // Get single user
-  const getUser = async () => {
-    setLoading();
-
-    const response = await fetch(`${GITHUB_URL}/users/{login}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-
-    if (response.status === 404) {
-      window.location = "/notfound";
-    } else {
-      const data = await response.json();
-      dispatch({
-        type: "GET_USER",
-        payload: data,
-      });
-    }
-  };
-
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
-        searchUsers,
-        clearUsers,
-        getUser,
+        ...state,
+        dispatch,
       }}
     >
       {children}
