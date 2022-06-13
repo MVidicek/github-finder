bdc5eee8371be4462952f0c7211e93f289438811
MVidicek <mvidicek@protonmail.com>
2022-06-13 18:39:53 +0200

    Alert component

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,10 +1,12 @@
 import { useState, useContext } from "react";
 import GithubContext from "../../context/github/GithubContext";
+import AlertContext from "../../context/alert/AlertContext";
 
 function UserSearch() {
   const [text, setText] = useState("");
 
   const { users, searchUsers, clearUsers } = useContext(GithubContext);
+  const { setAlert } = useContext(AlertContext);
 
   const handleChange = (e) => {
     setText(e.target.value);
@@ -14,12 +16,12 @@ function UserSearch() {
     e.preventDefault();
 
     if (text === "") {
-      return;
+      setAlert("Please enter a username", "error");
+    } else {
+      searchUsers(text);
+
+      setText("");
     }
-
-    searchUsers(text);
-
-    setText("");
   };
 
   return (
